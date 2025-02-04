e90228498a7cfe670bf971ffda1a42f593e2aea2
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-04 18:47:12 -0300

    style: add custom fonts and color palette to Tailwind CSS config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const tailwindConfig = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "var(--font-quicksand)",
+      secondary: "var(--font-geist-mono)",
+      alternative: "var(--font-spectral)",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#A57865", // Mocha Mousse
+        secondary: "#939684", // Laurel Oak
+        writingDark: "#161616", // Black
+        writingLight: "#FFFFFF", // White
+        accent: "#1E6F85", // Tapestry
+        background: "#F0F0F0", // White Smoke
       },
     },
   },
   plugins: [],
 };
+
+export default tailwindConfig;
