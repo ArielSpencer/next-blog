2e38db5f07dcc4fd8b97e3de89d0d4c6b29b10b0
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-04 18:52:51 -0300

    feat: integrate Header, Footer, and global layout configuration into layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Spectral, Quicksand } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,18 +13,36 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const spectralFont = Spectral({
+  variable: "--font-spectral",
+  subsets: ["latin"],
+  weight: ["200", "400", "700"],
+  style: ["normal", "italic"],
+});
+
+// Importando a fonte Quicksand
+const quicksandFont = Quicksand({
+  variable: "--font-quicksand",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"], // Definindo pesos disponíveis
+  style: ["normal"], // Quicksand não possui versão itálica
+});
+
 export const metadata = {
-  title: " Ariel Spencer Blog | Tech, Programação, UX Design e SEO",
-  description: "Explore artigos e tutoriais sobre programação, UX design e SEO. Aprenda dicas práticas, melhores práticas e tendências do mundo tech por Ariel Spencer.",
+  title: "Ariel Spencer Blog | Tech, Programação, UX Design e SEO",
+  description:
+    "Explore artigos e tutoriais sobre programação, UX design e SEO. Aprenda dicas práticas, melhores práticas e tendências do mundo tech por Ariel Spencer.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`bg-[#F6F3ED] ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-background font-primary text-writingDark tracking-wider font-medium ${geistSans.variable} ${geistMono.variable} ${spectralFont.variable} ${quicksandFont.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
