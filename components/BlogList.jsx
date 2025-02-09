a0f74636e17265b116c3d751446e97b92cc19abf
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-08 22:55:28 -0300

    feat: integrate API to fetch blogs from database in BlogList component

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,10 +1,22 @@
 import { blog_data } from "@/assets/assets";
 import BlogItem from "./BlogItem";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const BlogList = () => {
 
   const [menu, setMenu] = useState("All");
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get("/api/blog");
+    setBlogs(response.data.blogs);
+    console.log(response.data.blogs);
+  }
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [])
 
   return (
     <div>
@@ -42,8 +54,8 @@ const BlogList = () => {
       <div
         className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24"
       >
-        {blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => {
-          return <BlogItem key={index} id={item.id} image={item.image} title={item.title} description={item.description} category={item.category} alt={item.alt} />
+        {blogs.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => {
+          return <BlogItem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category} alt={item.alt} />
         })}
       </div>
     </div>
